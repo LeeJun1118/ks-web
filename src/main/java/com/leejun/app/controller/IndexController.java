22b53a38ac9e3e4b5958107a95e1ae1d1e1548f8
ljun1118 <ejun1118@gmail.com>
2020-04-20 18:48:43 +0900

    IndexController 추가 -20200413수업

--- src/main/java/com/leejun/app/controller/IndexController.java
+++ src/main/java/com/leejun/app/controller/IndexController.java
@@ -8,18 +8,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
-
     private final StudentRepository studentRepository;
 
     public IndexController(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
     }
 
+
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model) {
         Iterable<Student> students = studentRepository.findAll();
 
-        model.addAttribute("students",students);
+        model.addAttribute("students", students);
         return "index";
     }
 }
